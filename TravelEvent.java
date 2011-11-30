52dedf398ce67a73312f6c30e7e891b72ba6ecc7
Nick DeRossi <ncp22@wildcats.unh.edu>
2011-11-30 01:39:33 -0500

    Added startlocation field and accessor

--- TravelEvent.java
+++ TravelEvent.java
@@ -10,23 +10,28 @@ import java.util.*;
 public class TravelEvent extends Event{
     //-----------------------Instance Variables------------------------
     protected String destination;       //Destination name.
+    protected String startLocation;     //Start location name
     
     //Constructor
-    public TravelEvent(String n, GregorianCalendar s, GregorianCalendar e,
-            String d)
+    public TravelEvent(String n, String st, String d, GregorianCalendar s,
+           GregorianCalendar e )
     {
         super(n, s, e);
         destination = d;
+        startLocation = st;
     }
     
     //Returns  the destination name.
     public String getDestination() { return destination; }
+
+    //Returns the startLocation name
+    public String getStartLocation() { return startLocation; }
     
     //Returns string representation of the TravelEvent.
     public String toString()
     {
         String s ="";
-        s += name + " to " + destination + " " 
+        s += name + " from " + startLocation + " to " + destination + " " 
                 + startTime.get(Calendar.HOUR_OF_DAY) + ":" 
                 + startTime.get(Calendar.MINUTE) + " to "
                 + endTime.get(Calendar.HOUR_OF_DAY) + ":" 
